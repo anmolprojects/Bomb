49781968c8ab52a9924d0289a3c133e3ff15c67f
anmol-agrawal-dev <anmolagrawaledu@gmail.com>
2025-12-18 11:29:38 +0530

    Add files via upload

---
+++ bomb.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class bomb
+{public static void  main(String ag[])throws Exception
+{Scanner sc=new Scanner(System.in);
+System.out.println("Rows and columns of land and water");
+int r=sc.nextInt();
+int c=sc.nextInt();
+String arr[][]=new String[r][c];
+System.out.println("Enter land and water array");
+for(int i=0;i<r;i++)
+{for(int x=0;x<c;x++)
+{
+arr[i][x]=sc.next();
+}
+}
+System.out.println("Enter position of the bomb");
+int rb=sc.nextInt();
+int cb=sc.nextInt();
+rb=rb-1;
+cb=cb-1;
+arr[rb][cb]="#";
+System.out.println("Original array");
+for(int i=0;i<r;i++)
+{for(int x=0;x<c;x++)
+{System.out.print(arr[i][x]+" ");
+}
+System.out.println();
+}
+
+for(day=1;day<=7;day++)
+{s=s+1;
+day=Thursday;
+if(year%4==0)
+}
+}
+}
+			
+
